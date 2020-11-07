ae4218b37e54e14cf6b1d200d6cd603a1b3d50a3
Tiar Agisti <tiar.agisti@gmail.com>
2020-11-07 16:01:24 +0700

    submit message struct for mapping table and request json submit message

--- models/messageModel/messageModel.go
+++ models/messageModel/messageModel.go
@@ -1,1 +1,18 @@
 package messageModel
+
+import "github.com/jinzhu/gorm"
+
+type (
+	//MessageDatas - mapping table message_datas
+	MessageDatas struct {
+		gorm.Model
+		UserID  string `gorm:"column:user_id"`
+		Message string `gorm:"column:message"`
+	}
+
+	//MessageRequest - struct for request message
+	MessageRequest struct {
+		UserID  int    `json:"user_id"`
+		Message string `json:"message"`
+	}
+)
